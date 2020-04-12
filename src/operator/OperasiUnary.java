5ad14a307dfbfdc40d1c03e7a74e5b7189581832
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-12 13:19:56 +0800

    Operasi Unary

---
+++ src/operator/OperasiUnary.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperasiUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i+=3;
+        System.out.println(i);
+        i+=3;
+        System.out.println(i);
+        i*=3;
+        System.out.println(i);
+        i*=3;
+        System.out.println(i);
+    }
+}
