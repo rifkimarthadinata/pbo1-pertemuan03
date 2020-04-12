a4ffc0fd8437dd2a6c9b2385c0aad5d6a5b5c1c9
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-12 14:06:03 +0800

    Percabangan switch jalanjalan

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,25 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanjalan=3;
+        switch(jalanjalan){
+            case 1:
+                System.out.println("urang bukah");
+                break;
+            case 2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("Ubat Nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            default:
+                System.out.println("mahluk halus");
+                break;
+
+        }
+    }
+}
