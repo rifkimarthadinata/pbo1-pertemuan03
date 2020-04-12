31d4914c1d392c090a261e8ae888a1fc20ff25ca
rifkimarthadinata <62433657+rifkimarthadinata@users.noreply.github.com>
2020-04-12 13:08:01 +0800

    Operasi Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -6,5 +6,12 @@ public class OperatorBinary {
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.29;
 
-
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
